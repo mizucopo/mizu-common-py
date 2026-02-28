80aa47fadfe3c11fc29ddb10e73e774599ae13ca
mizu <mizu.copo@gmail.com>
2026-02-28 21:06:34 +0900

    feat: Discord Webhook通知クライアントを追加

--- src/mizu_common/__init__.py
+++ src/mizu_common/__init__.py
@@ -2,7 +2,9 @@
 
 from mizu_common.backup_manager import BackupManager
 from mizu_common.constants.google_scope import GoogleScope
+from mizu_common.discord_client import DiscordClient
 from mizu_common.exceptions.already_running_error import AlreadyRunningError
+from mizu_common.exceptions.discord_webhook_error import DiscordWebhookError
 from mizu_common.exceptions.stale_lock_error import StaleLockError
 from mizu_common.exceptions.youtube_api_error import YouTubeApiError
 from mizu_common.exceptions.youtube_http_error import YouTubeHttpError
@@ -11,6 +13,7 @@ from mizu_common.google_drive_provider import GoogleDriveProvider
 from mizu_common.google_oauth_client import GoogleOAuthClient
 from mizu_common.lock_manager import LockManager
 from mizu_common.logging_configurator import LoggingConfigurator
+from mizu_common.models.discord_embed import DiscordEmbed
 from mizu_common.models.youtube_video_info import YouTubeVideoInfo
 from mizu_common.youtube_client import YouTubeClient
 
@@ -22,8 +25,10 @@ __all__ = [
     "GoogleOAuthClient",
     "GoogleDriveProvider",
     "YouTubeClient",
+    "DiscordClient",
     # データモデル
     "YouTubeVideoInfo",
+    "DiscordEmbed",
     # 定数
     "GoogleScope",
     # 例外
@@ -32,4 +37,5 @@ __all__ = [
     "YouTubeNetworkError",
     "StaleLockError",
     "AlreadyRunningError",
+    "DiscordWebhookError",
 ]
